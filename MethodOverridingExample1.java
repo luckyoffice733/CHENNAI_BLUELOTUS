d62e68948ccf5f7bdb5bb879b98151f64e859cf4
lucky733 <luckyoffice733@gmail.com>
2024-12-27 09:43:37 +0530

    overridining

--- MethodOverridingExample1.java
+++ MethodOverridingExample1.java
@@ -1,26 +1,30 @@
 package com.training;
 
 class Parent{
-	
-	void show(String msg) {
+	 void show(String msg) {
 		System.out.println("FROM PARENT CLASS BY : "+msg);
 	}
-	
 	public void methodTwo() {
 		System.out.println("we are in the methodTwo of Parent class");
 	}
-	
+	public Object sayHello() {
+		return "welcome to java";
+	}
 }
 class Child extends Parent{
-	
 	public void methodOne() {
 		System.out.println("we are in the methodOne of child class");
 	}
-	
-    @Override
-	void show(String msg) {
-		System.out.println("FROM Child CLASS BY : "+msg);
+    @Override  
+	void show(String m) {
+		System.out.println("FROM Child CLASS BY : "+m);
 	}	
+    
+    @Override
+    public String sayHello() { //co-variant return type
+    	return "Hello world ";
+    }
+    
 }
 public class MethodOverridingExample1 {
      public static void main(String[] args) {
@@ -29,5 +33,8 @@ public class MethodOverridingExample1 {
 		
 		chobj.methodTwo(); //methodTwo()
 		chobj.methodOne();// methodOne()
+		
+		String msg =chobj.sayHello();//
+		System.out.println(msg);
 	}
 }
